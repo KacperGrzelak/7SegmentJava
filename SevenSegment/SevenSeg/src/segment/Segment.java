64a8bb01f630fa15f0d69088ffd99cf20309ce91
KacperGrzelak <58523750+KacperGrzelak@users.noreply.github.com>
2020-04-14 21:58:29 +0200

    Add files via upload

---
+++ SevenSegment/SevenSeg/src/segment/Segment.java
@@ -0,0 +1,66 @@
+package segment;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+
+
+public class Segment extends JPanel {
+
+    private Segment[] segment = new Segment[8];
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4980210296562972446L;
+	
+	private Rectangle[] rect = {
+			new Rectangle(10,10,105,10),
+			new Rectangle(115,20,10,115),
+			new Rectangle(115,145,10,115),
+			new Rectangle(10,260,105,10),
+			new Rectangle(00,145,10,115),
+			new Rectangle(00,20,10,115),
+			new Rectangle(10,135,105,10),
+                        new Rectangle(130,260,13,13)
+		};
+	
+	private JPanel[] pnlSeg;
+	
+	public JPanel getPanelSeg(int index)
+	{
+		return this.pnlSeg[index];
+	}
+	
+	public Segment() {
+		super();
+		initSegment(new Dimension(90,90));
+		
+	}
+
+	public Segment(Dimension dim) {
+		super();
+		initSegment(dim);
+	}
+
+	private void initSegment(Dimension dim)
+	{
+		this.setSize(dim);
+		this.pnlSeg = new JPanel[8];
+                this.setOpaque(false);
+		
+		for (int j = 0; j < 8; j++)
+		{
+			this.pnlSeg[j] = new JPanel();
+			this.pnlSeg[j].setBounds(rect[j]);
+			this.pnlSeg[j].setLayout(null);
+		}
+                
+		
+	}
+
+	public Rectangle getRect(int index)
+	{
+		return rect[index];
+	}
+}
---
+++ SevenSegment/SevenSeg/src/segment/UI.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segment;
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import java.awt.FlowLayout;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+
+public class UI extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4621924450652779205L;
+	private Segment[] segment = new Segment[8];
+	private  int value;
+	public UI()
+	{
+		setSize(880, 300);
+		FlowLayout layout = new FlowLayout();
+		layout.setHgap(8);
+		layout.setVgap(0);
+		layout.setAlignment(FlowLayout.LEFT);
+		setLayout(layout);
+		Dimension dim = new Dimension();
+		
+		for (int i = 0; i < 3; i++)
+		{
+			dim.setSize(getSize().width/6, 300);
+			segment[i] = new Segment(dim);
+			segment[i].setName("Segment #"+i);
+			segment[i].setPreferredSize(dim);
+			segment[i].setLayout(null);
+			
+			for (int j = 0; j < 8; j++)
+			{
+				segment[i].getPanelSeg(j).setBackground((i < 1) ? Color.RED : (i < 2) ? Color.RED : (i < 3) ? Color.RED : Color.WHITE);
+				segment[i].getPanelSeg(j).setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+				segment[i].add(segment[i].getPanelSeg(j));
+			}
+			add(segment[i]);
+		}
+                
+	}
+        
+        
+        public void setValue (int value){
+            try{
+                
+                if (value >= 0 && value < 1000)
+            {
+                for (int i = 0; i < 8; i++){
+                segment[0].getPanelSeg(i).setVisible((Binary.CLOCKBIN[Binary.getNthDigit(value, 10, 3)][i] == true) ? true : false);
+                segment[1].getPanelSeg(i).setVisible((Binary.CLOCKBIN[Binary.getNthDigit(value, 10, 2)][i] == true) ? true : false);
+                segment[2].getPanelSeg(i).setVisible((Binary.CLOCKBIN[Binary.getNthDigit(value, 10, 1)][i] == true) ? true : false);
+            }}
+            else if( value < 0 && value >= -99){
+                for (int i = 0; i < 8; i++){
+                segment[0].getPanelSeg(i).setVisible((Binary.LETTERBIN[Binary.getMDigit()][i] == true) ? true : false);
+                int val = value > 0 ? value : -value;
+                segment[1].getPanelSeg(i).setVisible((Binary.CLOCKBIN[Binary.getNthDigit(val, 10, 2)][i] == true) ? true : false);
+                segment[2].getPanelSeg(i).setVisible((Binary.CLOCKBIN[Binary.getNthDigit(val, 10, 1)][i] == true) ? true : false);
+            }}
+            else {
+                setError();
+            }
+            
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("error");
+                
+            } 
+        }
+        
+        public void setError()
+        {
+            for (int i = 0; i < 8; i++){
+                segment[0].getPanelSeg(i).setVisible((Binary.LETTERBIN[Binary.getEDigit()][i] == true) ? true : false);
+                segment[1].getPanelSeg(i).setVisible((Binary.LETTERBIN[Binary.getRDigit()][i] == true) ? true : false);
+                segment[2].getPanelSeg(i).setVisible((Binary.LETTERBIN[Binary.getRDigit()][i] == true) ? true : false);
+                }
+        }
+        public int getValue()
+        {
+            
+            
+            
+            return value;
+        }
+         
+}
